b1d9a9a392a56ddae031fea8800a614c162835fa
razqyaqil <razqyaqil2@gmail.com>
2022-03-24 21:12:28 +0700

    Add project files.

---
+++ tpmodul5_1302202072/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace tpmodul5_1302202072 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
